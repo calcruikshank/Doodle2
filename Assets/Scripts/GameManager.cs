15eb9360bf7fa090992aa169b3d786647b581695
calcruikshank <calcruikshank@gmail.com>
2022-07-31 20:18:23 -0600

    colors workin

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -46,35 +46,35 @@ public class GameManager : MonoBehaviour
     }
     public void SetColorOrange()
     {
-        lineColor = new Color(.93f, .11f, .14f);
+        lineColor = new Color(.97f, .58f, .11f);
     }
     public void SetColorYellow()
     {
-        lineColor = new Color(.93f, .11f, .14f);
+        lineColor = new Color(1f, .95f, 0f);
     }
     public void SetColorGreen()
     {
-        lineColor = new Color(.93f, .11f, .14f);
+        lineColor = new Color(0f, .65f, .32f);
     }
     public void SetColorLightBlue()
     {
-        lineColor = new Color(.93f, .11f, .14f);
+        lineColor = new Color(0f, .68f, .94f);
     }
     public void SetColorDarBlue()
     {
-        lineColor = new Color(.93f, .11f, .14f);
+        lineColor = new Color(.18f, .19f, .57f);
     }
     public void SetColorPurple()
     {
-        lineColor = new Color(.93f, .11f, .14f);
+        lineColor = new Color(.57f, .15f, .56f);
     }
     public void SetColorPink()
     {
-        lineColor = new Color(.93f, .11f, .14f);
+        lineColor = new Color(.94f, .31f, .53f);
     }
     public void SetColorBrown()
     {
-        lineColor = new Color(.93f, .11f, .14f);
+        lineColor = new Color(.46f, .30f, .16f);
     }
     public void SetColorBlack()
     {
@@ -82,7 +82,7 @@ public class GameManager : MonoBehaviour
     }
     public void SetColorGray()
     {
-        lineColor = Color.black;
+        lineColor = new Color(.51f, .51f, .52f);
     }
     public void SetColorWhite()
     {
